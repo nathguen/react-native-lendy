890e09cda72d8a3dd522cd2bdc376a1820fac06f
Nathan <nathan.guenther.web@gmail.com>
2023-03-25 12:09:53 -0600

    fix(Input): expose Input styles

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 export { default as Button } from './Button/index';
 export * from './Button/index';
 export { default as Input } from './Input';
+export { getInputStyles, InputProps } from './Input';
 export { default as ScreenTabs } from './ScreenTabs/index';
 export { default as Text } from './Text/index';
 export * from './Text/index';
